cbb2d73ddb673f4338a57b9a1394885278e1b6e7
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-24 00:54:56 -0400

    Add function to get total number of tabs opened
    Doing chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_NONE})
    returns all chrome tabs for all windows apparently.
    Took me a while since I over throught/engineered it.

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
-chrome.action.setBadgeText({text: "1+"});
+/*===== Tab(s) =====*/
+
+// Returns total list of tabs from all windows
+async function getTotalTabsOpened() {
+    let num = await chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_NONE});
+    return num.length;
+}
